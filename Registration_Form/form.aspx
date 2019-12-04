e10f77790a21241ca88afacded3ca9ff8044a1a6
vishalanand768 <vishalanand768@yahoo.com>
2019-12-04 19:23:50 +0530

    first commit

---
+++ Registration_Form/form.aspx
@@ -0,0 +1,139 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="form.aspx.cs" Inherits="Registration_Form.form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type ="text/javascript">
+        function validation() {
+            var dabba = "";
+            dabba += checkfname();
+            dabba += checklname();
+          
+            if (dabba == "") {
+                return true;
+            }
+            else {
+                alert(dabba);
+                return false;
+            }
+        }
+        function checkfname() {
+            var TB = document.getElementById('txtfname');
+            if (TB.value == "") {
+                return 'Plz Enter your first name !!\n';
+            }
+
+            else {
+                return "";
+            }
+        }
+        function checklname() {
+            var Tb = document.getElementById('txtlname');
+            if (Tb.value == "") {
+                return 'Plz Enter your Last name!!\n';
+            }
+            else {
+                return "";
+            }
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>First Name  : </td>
+                <td><asp:TextBox ID="txtfname" runat="server"></asp:TextBox></td>
+            </tr>
+
+             <tr>
+                <td>Last Name  : </td>
+                <td><asp:TextBox ID="txtlname" runat="server"></asp:TextBox></td>
+            </tr>
+
+             <tr>
+                <td>Gender  : </td>
+                <td><asp:RadioButtonList ID="rblgen" runat="server" RepeatColumns="3">
+                    <asp:ListItem Text="Male" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Female" Value="2"></asp:ListItem>
+                    <asp:ListItem Text="Other" Value="3"></asp:ListItem>
+                    </asp:RadioButtonList></td>
+            </tr>
+
+             <tr>
+                <td>Country  : </td>
+                <td><asp:DropDownList ID="ddlcountry" runat="server"></asp:DropDownList></td>
+            </tr>
+
+             <tr>
+                <td>Qualification  : </td>
+                <td><asp:DropDownList ID="ddlqual" runat="server"></asp:DropDownList></td>
+            </tr>
+
+             <tr>
+                <td>Blood Group  : </td>
+                <td><asp:DropDownList ID="ddlbldgp" runat="server"></asp:DropDownList></td>
+            </tr>
+
+             <tr>
+                <td></td>
+                <td><asp:Button ID="btnsave" runat="server" Text="Save" OnClientClick=" return validation()" OnClick="btnsave_Click" ></asp:Button></td>
+            </tr>
+
+             <tr>
+                <td></td>
+                <td><asp:GridView ID="grd" runat="server" OnRowCommand="grd_RowCommand" AutoGenerateColumns="false">
+                  <Columns>
+                        <asp:TemplateField HeaderText="First Name">
+                            <ItemTemplate>
+                                <%#Eval("Fisrt_Name") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="Last Name">
+                            <ItemTemplate>
+                                <%#Eval("Last_Name") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="Gender">
+                            <ItemTemplate>
+                                 <%#Eval("Gender").ToString()=="1" ? "male" :Eval("gender").ToString()=="2" ? "female" : "others"  %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="Country">
+                            <ItemTemplate>
+                                <%#Eval("cname") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="Qualification">
+                            <ItemTemplate>
+                                <%#Eval("qname") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="Blood Group">
+                            <ItemTemplate>
+                                <%#Eval("bname") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                      <asp:TemplateField HeaderText="Action">
+                        <ItemTemplate>
+                            <asp:Button ID="btndelete" runat="server" Text="Delete" CommandName="A" CommandArgument='<%#Eval("id") %>' />
+                            <asp:Button ID="btnedit" runat="server" Text="Edit" CommandName="B" CommandArgument='<%#Eval("id") %>' />
+                       </ItemTemplate>
+                       </asp:TemplateField>
+
+                     </Columns>
+                    </asp:GridView></td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Registration_Form/form.aspx.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Registration_Form
+{
+    public partial class form : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["xyz"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindCountry();
+                BindQualification();
+                BindBloodGroup();
+                BindRegistration();
+            }
+        }
+
+        public void Clear()
+        {
+            txtfname.Text = "";
+            txtlname.Text = "";
+            rblgen.ClearSelection();
+            ddlcountry.SelectedValue = "0";
+            ddlqual.SelectedValue = "0";
+            ddlbldgp.SelectedValue = "0";
+            BindRegistration();
+        }
+
+        public void BindCountry()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_getcountrydata", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddlcountry.DataValueField = "Cid";
+            ddlcountry.DataTextField = "Cname";
+            ddlcountry.DataSource = dt;
+            ddlcountry.DataBind();
+            ddlcountry.Items.Insert(0, new ListItem("--Select--", "0"));
+        }
+
+        public void BindQualification()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_QualBind", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddlqual.DataValueField = "Qid";
+            ddlqual.DataTextField = "Qname";
+            ddlqual.DataSource = dt;
+            ddlqual.DataBind();
+            ddlqual.Items.Insert(0, new ListItem("--Select--", "0"));
+        }
+
+        public void BindBloodGroup()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_BldGrpBind", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddlbldgp.DataValueField = "Bid";
+            ddlbldgp.DataTextField = "Bname";
+            ddlbldgp.DataSource = dt;
+            ddlbldgp.DataBind();
+            ddlbldgp.Items.Insert(0, new ListItem("--Select--", "0"));
+        }
+        public void BindRegistration()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("RegDisplayData", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+
+            grd.DataSource = dt;
+            grd.DataBind();
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("RegDisplay_Insert", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Fisrt_Name", txtfname.Text);
+                cmd.Parameters.AddWithValue("@Last_Name", txtlname.Text);
+                cmd.Parameters.AddWithValue("@Gender", rblgen.SelectedValue);
+                cmd.Parameters.AddWithValue("@Country", ddlcountry.SelectedValue);
+                cmd.Parameters.AddWithValue("@Qualification", ddlqual.SelectedValue);
+                cmd.Parameters.AddWithValue("@Blood_Group", ddlbldgp.SelectedValue);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindRegistration();
+                Clear();
+
+            }
+
+            else if (btnsave.Text == "Update") 
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_update", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", ViewState["pp"]);
+                cmd.Parameters.AddWithValue("@Fisrt_Name", txtfname.Text);
+                cmd.Parameters.AddWithValue("@Last_Name", txtlname.Text);
+                cmd.Parameters.AddWithValue("@Gender", rblgen.SelectedValue);
+                cmd.Parameters.AddWithValue("@Country", ddlcountry.SelectedValue);
+                cmd.Parameters.AddWithValue("@Qualification", ddlqual.SelectedValue);
+                cmd.Parameters.AddWithValue("@Blood_Group", ddlbldgp.SelectedValue);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindRegistration();
+                Clear(); 
+            }
+        }
+
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_delete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindRegistration();
+            }
+            else if (e.CommandName == "B")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_edit", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+                txtfname.Text = dt.Rows[0]["Fisrt_Name"].ToString();
+                txtlname.Text = dt.Rows[0]["Last_Name"].ToString();
+                rblgen.SelectedValue = dt.Rows[0]["Gender"].ToString();
+                ddlcountry.SelectedValue = dt.Rows[0]["Country"].ToString();
+                ddlqual.SelectedValue = dt.Rows[0]["Qualification"].ToString();
+                ddlbldgp.SelectedValue = dt.Rows[0]["Blood_Group"].ToString();
+                btnsave.Text = "Update";
+                ViewState["pp"] = e.CommandArgument;
+            }
+        }
+    }
+}
